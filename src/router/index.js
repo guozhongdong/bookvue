18fdb6a2acaeca997b29db32ca69a900074fd3a8
guozhongdong <guozhongdong>
2020-03-31 09:10:14 +0800

    feat 设置导航栏

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import pageOne from '@/view/pageOne'
-import pageTwo from '@/view/pageTwo'
+import PageOne from '@/view/pageOne'
+import PageTwo from '@/view/pageTwo'
+import PageThree from '@/view/pageThree'
+import PageFour from '@/view/pageFour'
+import Index from '@/view/index'
 
 Vue.use(Router)
 export default new Router({
@@ -10,18 +12,38 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: '导航一',
+      component: Index,
+      children: [
+        {
+          path: '/pageOne',
+          name: '页面一',
+          component: PageOne
+        },
+        {
+          path: '/pageTwo',
+          name: '页面二',
+          component: PageTwo
+        }
+      ]
     },
     {
-      path: '/pageOne',
-      name: 'pageOne',
-      component: pageOne
+      path: '/navigation',
+      name: '导航二',
+      component: Index,
+      children: [
+        {
+          path: '/pageThree',
+          name: '页面三',
+          component: PageThree
+        },
+        {
+          path: '/pageFour',
+          name: '页面四',
+          component: PageFour
+        }
+      ]
     },
-    {
-      path: '/pageTwo',
-      name: 'pageTwo',
-      component: pageTwo
-    }
+
   ]
 })
